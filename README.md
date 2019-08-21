37d465116437c1ae28a91b39d05d18d31ea71027
Junicchi <24779257+KebabLord@users.noreply.github.com>
2019-08-22 00:12:05 +0300

    switched to functions instead of if statements

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # Mini Termux SSH Controller Script
-To execute the script you should use `source <script>` command in order to store variables on that terminal session
-You can also add an alias to `.bash_aliases` as `alias termux="source PATH_TO_SCRIPT.sh"`
 
 ```
 λ termux --help
--- termux-mini.sh
+++ termux-mini.sh
@@ -1,15 +1,31 @@
-PHONE_MAC=<YOUR_PHONES_MAC_ADRESS> # Mac adresses are static
+#!/usr/bin/bash
+PHONE_MAC="<YOUR_PHONES_MAC_ADRESS>" # Mac adresses are static
 PHONE_KEY="~/.ssh/<YOUR_SECRET_KEY>" # Secret key to use while connect to phone
 
-if [ -z $PHONE_IP ]
-then
+updateIP() { #GET IP ADRESS OF THE PHONE FROM BELOW MAC ADRESS
+	printf "$1 updating the ip adress...\r"
 	nmap -sn 192.168.1.0/24>/dev/null
 	PHONE_IP=`for ((i=1; i<=255; i++));do arp -a 192.168.1.$i; done | grep $PHONE_MAC | awk '{print $2}' | sed -e 's/(//' -e 's/)//'`
+	echo $PHONE_IP>/tmp/phone_ip.tmp
+	printf "Updated!                                               \n"
+}
+
+if [ -e /tmp/phone_ip.tmp ]
+	then
+		PHONE_IP=`cat /tmp/phone_ip.tmp`
+		if [[ `arp -a $PHONE_IP` == *'no'* ]] # if ip is unavailable
+			then updateIP "Renewing needed,"
+		fi
+	else									  # if no ip saved before
+		updateIP "Launching for the first time,"
 fi
+
 PHONE_SSH="ssh $PHONE_IP -p 8022 -i $PHONE_KEY"
 
 if [ -z $1 ]
-	then eval $PHONE_SSH
+	then
+		printf "Connecting to SSH...\r"
+		eval $PHONE_SSH
 else
 	if [ $1 == "cp-set" ]
 		then eval "$PHONE_SSH termux-clipboard-set '$2'" &
@@ -25,11 +41,13 @@ else
 			scp -P 8022 -i $PHONE_KEY $2 $PHONE_IP:./tmp/
 			eval "$PHONE_SSH termux-share -a send ./tmp/*"
 	elif [ $1 == "mount" ]
-		then
-			sshfs -p 8022 -o IdentityFile=$PHONE_KEY $PHONE_IP:$2 $3
+		then sshfs -p 8022 -o IdentityFile=$PHONE_KEY $PHONE_IP:$2 $3
+	elif [ $1 == "scan" ]
+		then updateIP "Manually"
 	else
 		echo "Mini Termux Controller - github/Kebablord
-scan		    *  scan's ports to find phone's ip adress
+
+scan                *  update phone's ip,u can use it if you encounter a problem
 cp-set <string>     -  copies the string to phone's clipboard
 cp-get              -  return the string from phone's clipboard
 pull  <src> <dest>  -  pull the file from phone src to dest
